be4a2ee6da68d321388e6252b57b14b335860832
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2025-05-21 17:51:15 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Blog from "./pages/myBlog";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Tech & Trends Journal</h1>
+      </header>
+      <main>
+        <Blog />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
